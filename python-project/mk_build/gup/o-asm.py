c41386ff3d9b6838c23cd607f0c9bce2316c3dc8
Fritz Mahnke <fritz@mahnke.tech>
2024-05-28 10:20:43 -0700

    convert o-asm builder to convention

--- python-project/mk_build/gup/o-asm.py
+++ python-project/mk_build/gup/o-asm.py
@@ -1,31 +1,36 @@
 #!/usr/bin/env python
 
+import os
 import sys
 
 import mk_build
 from mk_build import *
 
-output = sys.argv[1]
-target = sys.argv[2]
 
-print(f'o-asm  {target}')
+def main(output, target, env=os.environ) -> int:
+    print(f'o-asm  {target}')
 
-m_asm_ppflags = mk_build.environ('M_ASM_PPFLAGS', True)
-deps = mk_build.environ('DEPS', True)
+    m_asm_ppflags = mk_build.environ('M_ASM_PPFLAGS', True)
+    deps = mk_build.environ('DEPS', True)
 
-if deps != '':
-    deps = deps.split()
-    gup(deps)
+    if deps != '':
+        gup(deps.split())
 
-source = path(top_source_dir(), path(target).with_suffix('.asm'))
+    source = path(top_source_dir(), path(target).with_suffix('.asm'))
 
-target_source_dir = path_dir(source)
+    target_source_dir = path_dir(source)
 
-args = ['nasm', '-f', 'elf32', '-g', '-w+error', '-i', target_source_dir,
-    m_asm_ppflags, '-o', output, source]
+    args = ['nasm', '-f', 'elf32', '-g', '-w+error', '-i', target_source_dir,
+        m_asm_ppflags, '-o', output, source]
 
-gup(source)
+    gup(source)
 
-result = run(args)
+    result = run(args)
 
-sys.exit(result.returncode)
+    return result.returncode
+
+
+if __name__ == '__main__':
+    result = main(output, target)
+
+    sys.exit(result)
