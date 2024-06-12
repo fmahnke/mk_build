9d61381e8e2e1183c02cbdc556690c5d762b460d
Fritz Mahnke <fritz@mahnke.tech>
2024-06-12 09:46:27 -0700

    add C to object builder

---
+++ python-project/mk_build/gup/o-c.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import mk_build
+from mk_build import *
+
+
+def main(output, target, env=os.environ) -> int:
+    print(f'o-c  {target}')
+
+    ppflags = mk_build.environ('M_C_PPFLAGS', True).split()
+    flags = mk_build.environ('M_C_FLAGS', True).split()
+    deps = mk_build.environ('DEPS', True)
+
+    if deps != '':
+        gup(deps.split())
+
+    source = path(top_source_dir(), path(target).with_suffix(''))
+
+    # GCC manual recommends using -mgeneral-regs-only when functions are
+    # no_caller_saved_registers
+
+    cc = ['clang', '-target', 'i386-elf', '-march=i386', '-mgeneral-regs-only']
+
+    args = cc + ppflags + flags + ['-c', '-o', output, source]
+
+    gup(source)
+
+    result = run(args)
+
+    return result.returncode
+
+
+if __name__ == '__main__':
+    result = main(output, target)
+
+    sys.exit(result)
