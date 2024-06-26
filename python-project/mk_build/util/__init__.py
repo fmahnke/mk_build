c918ddb1a9350e608ad5b41b7db27379a73b373c
Fritz Mahnke <fritz@mahnke.tech>
2024-06-26 08:57:54 -0700

    wsl detection

--- python-project/mk_build/util/__init__.py
+++ python-project/mk_build/util/__init__.py
@@ -1,1 +1,11 @@
+import os.path
+import platform
 
+
+def system():
+    # TODO may not actually need this
+
+    if os.path.isfile('/proc/sys/fs/binfmt_misc/WSLInterop'):
+        return 'wsl'
+    else:
+        return platform.system()
