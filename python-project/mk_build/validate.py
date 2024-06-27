03435f805ce1f08ebb755fe3b1d8ad42c62bf650
Fritz Mahnke <fritz@mahnke.tech>
2024-06-27 16:31:25 -0700

    add check_type validation. return instance from ensure_type

--- python-project/mk_build/validate.py
+++ python-project/mk_build/validate.py
@@ -5,8 +5,29 @@ from .message import invalid_type
 _T = TypeVar("_T")
 
 
-def ensure_type(obj, expected_type: Type[_T]) -> TypeGuard[_T]:
+def check_type(
+    obj,
+    expected_type: Type[_T],
+    message=None
+) -> TypeGuard[_T]:
     if not isinstance(obj, expected_type):
-        raise TypeError(invalid_type.format(obj, expected_type, type(obj)))
+        if message is None:
+            raise TypeError(invalid_type.format(obj, expected_type, type(obj)))
+        else:
+            raise TypeError(message)
 
     return True
+
+
+def ensure_type(
+    obj,
+    expected_type: Type[_T],
+    message=None
+) -> _T:
+    if not isinstance(obj, expected_type):
+        if message is None:
+            raise TypeError(invalid_type.format(obj, expected_type, type(obj)))
+        else:
+            raise TypeError(message)
+
+    return obj
