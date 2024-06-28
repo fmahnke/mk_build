48b0f6ebfb208332449fa91161d79950476f8e3c
Fritz Mahnke <fritz@mahnke.tech>
2024-06-28 13:13:08 -0700

    log detail

--- python-project/mk_build/log.py
+++ python-project/mk_build/log.py
@@ -5,9 +5,11 @@ import logging
 __all__ = ['DEBUG', 'INFO', 'WARNING', 'ERROR']
 
 
-_handler = logging.StreamHandler()
-_root_logger = logging.getLogger('root')
-_root_logger.addHandler(_handler)
+def set_detail(level: int) -> None:
+    if level < 0 or level > len(_detail_formatter) - 1:
+        raise Exception()
+    else:
+        set_formatter(_detail_formatter[level])
 
 
 def set_formatter(formatter) -> None:
@@ -16,3 +18,24 @@ def set_formatter(formatter) -> None:
 
 def set_level(level) -> None:
     _root_logger.setLevel(level)
+
+
+# Initialize the global logger
+
+_detail_str = [
+    '%(levelname)s:%(message)s',
+    '%(levelname)s:%(module)s::%(funcName)s:%(message)s',
+    '%(levelname)s:%(name)s:%(module)s::%(funcName)s:%(message)s'
+]
+
+_detail_formatter = [
+    logging.Formatter(_detail_str[0]),
+    logging.Formatter(_detail_str[1]),
+    logging.Formatter(_detail_str[2])
+]
+
+_handler = logging.StreamHandler()
+_root_logger = logging.getLogger('root')
+_root_logger.addHandler(_handler)
+
+set_detail(0)
